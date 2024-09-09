52c9b96e724c27d1f074ce579369359d65577f7a
Dar3173 <darlyn-1999@hotmail.com>
2024-09-09 10:38:23 -0500

    cambios en el menu

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const $openClose = document.getElementById("open-close"),
           $aside = document.getElementById("aside");
 
-    // Inicialmente ocultar el aside
+    
     $aside.classList.add("desplegar");
 
     $openClose.addEventListener("click", () => {
