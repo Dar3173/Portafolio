767c8b838bcbb06d7f8fc28d7d685226a0e7818b
Dar3173 <darlyn-1999@hotmail.com>
2024-09-13 17:36:00 -0500

    cambio de diseño en index, se agrego proyecto a web, y cambio en la animacion de la pag diseño

--- src/js/animation-show.js
+++ src/js/animation-show.js
@@ -1,22 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('.container-disenos .imagenes');
+    const containers = document.querySelectorAll('.container-disenos');
 
     const observerOptions = {
-      threshold: 0.1 // Se activa cuando el 10% del elemento está visible
+        threshold: 0.1 
     };
 
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.style.transform = 'scale(1)';
-        } else {
-          entry.target.style.transform = 'scale(0)';
-        }
-      });
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
     }, observerOptions);
 
-    // Observa cada imagen seleccionada
-    images.forEach(image => {
-      observer.observe(image);
+
+    containers.forEach(container => {
+        observer.observe(container);
     });
-  });
+});
