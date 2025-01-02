802421d03e40dfb600f420049c4c90cee45eec32
Dar3173 <darlyn-1999@hotmail.com>
2025-01-02 16:54:50 -0500

    cambios de diseño y estilos generales +footer

--- src/js/modalpopup.js
+++ src/js/modalpopup.js
@@ -1,18 +1,29 @@
-    window.onload = function() {
+window.onload = function() {
     var modal = document.getElementById("modalPopup");
-    modal.style.display = "block"; 
-    };
+    modal.style.display = "flex"; 
+    var modalContent = modal.querySelector('.modal-content');
+    setTimeout(() => {
+        modalContent.classList.add('show');
+    }, 100); // Añadir clase para animación
+};
 
-    var closeBtn = document.querySelector(".close");
-    closeBtn.onclick = function() {
+var closeBtn = document.querySelector(".close");
+closeBtn.onclick = function() {
     var modal = document.getElementById("modalPopup");
-    modal.style.display = "none";
-    };
+    var modalContent = modal.querySelector('.modal-content');
+    modalContent.classList.remove('show');
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 300); // Esperar que termine la animación
+};
 
-    window.onclick = function(event) {
+window.onclick = function(event) {
     var modal = document.getElementById("modalPopup");
+    var modalContent = modal.querySelector('.modal-content');
     if (event.target == modal) {
-    modal.style.display = "none"; 
+        modalContent.classList.remove('show');
+        setTimeout(() => {
+            modal.style.display = "none";
+        }, 300); // Esperar que termine la animación
     }
-    };
-
+};
